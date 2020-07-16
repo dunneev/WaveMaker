668a0a87c0b874214d8782a84daffbae8309ae8f
dunneev <dunneev@gmail.com>
2020-07-16 00:56:18 -0400

    Create oscillator files

---
+++ app/src/main/cpp/Oscillator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ev on 7/16/2020.
+//
+
+#include "Oscillator.h"
---
+++ app/src/main/cpp/Oscillator.h
@@ -0,0 +1,14 @@
+//
+// Created by Ev on 7/16/2020.
+//
+
+#ifndef WAVEMAKER_OSCILLATOR_H
+#define WAVEMAKER_OSCILLATOR_H
+
+
+class Oscillator {
+
+};
+
+
+#endif //WAVEMAKER_OSCILLATOR_H
