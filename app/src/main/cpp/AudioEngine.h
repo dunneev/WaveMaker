d7a29c16f8d33f7d7183d2a4211ad942b0effda0
dunneev <dunneev@gmail.com>
2020-07-18 13:10:13 -0400

    Complete audio engine

--- app/src/main/cpp/AudioEngine.h
+++ app/src/main/cpp/AudioEngine.h
@@ -2,13 +2,17 @@
 // Created by Ev on 7/16/2020.
 //
 
-#ifndef WAVEMAKER_AUDIOENGINE_H
-#define WAVEMAKER_AUDIOENGINE_H
-
+#include <aaudio/AAudio.h>
+#include "Oscillator.h"
 
 class AudioEngine {
+public:
+    bool start();
+    void stop();
+    void restart();
+    void setToneOn(bool isToneOn);
 
+private:
+    Oscillator oscillator_;
+    AAudioStream *stream_;
 };
-
-
-#endif //WAVEMAKER_AUDIOENGINE_H
